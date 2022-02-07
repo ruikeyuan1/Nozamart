7af3959b471f05d93bed0206addbb80b4f7f254c
Oliverherf <91316579+Oliverherf@users.noreply.github.com>
2022-02-07 19:21:48 +0100

    Pictures, some css and added a note for myself on loginpage

---
+++ Code/LoginPage/LoginPage.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" rel="stylesheet" type="text/css" href="header.css">
+    <title>Login</title>
+</head>
+<body>
+    <?php 
+    include 'header.html';
+    //C:\Users\olive\Documents\GitHub\ThreeMusketeer\Code\LoginPage\LoginPage.php
+    //include 'footer.html';
+    ?>
+    
+</body>
+</html>
