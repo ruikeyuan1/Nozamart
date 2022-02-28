dd1e17554e513c26bce2727a2fe5a5e0e4ec8c43
radoychev <62029581+radoychev@users.noreply.github.com>
2022-02-28 15:38:28 +0100

    Fixes
    css optimised

--- Code/IntegratedFolder/admin_page.php
+++ Code/IntegratedFolder/admin_page.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="css/"
     <title>Admin page</title>
 </head>
 <body>
@@ -12,20 +13,14 @@
     ?>
     <div class="main">
                <div class="main-content">
-                    <h1 class="main-section-heading">Please enter your verification code</h1>
-                    <form method="POST">
-    
-                    <!--Firstname-->
-                    <input type="text" placeholder="Enter code" name="verification_code" id="verification_code" required>
-    
-                   
-                    
-                    <!--Submit-->
-                    <input type="submit" class="registerbtn" name="register" value="Submit">
-    
-                    <div class="container signin">
-                        <p>Already have an account? <a href="#">Sign in</a>.</p>
+                    <h1>Admin control panel</h1>
+                    <div class="see-members">
+                        <a href="customers-overview.php">See all registered users</a>
                     </div>
+                    <div class="see-products">
+                        <a href="products-overview.php">Details about users</a>
+                    </div>
+                
                 </form>
                </div>
             </div>
