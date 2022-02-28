5a5cdc96e181298ed4e6e97c8a05de63f479917f
Oliverherf <91316579+Oliverherf@users.noreply.github.com>
2022-02-28 15:08:59 +0100

    Change db name to Noza

--- Code/IntegratedFolder/dataConnect.php
+++ Code/IntegratedFolder/dataConnect.php
@@ -1,13 +1,7 @@
 <!--Ruike Yuan Feb 2022-->
 <?php
     $host = "localhost";
-<<<<<<< HEAD
-    $user = "rick";
-    $pass = "";
-    $database = "Noza";
-=======
     $user = "root";
     $pass = "";
-    $database = "newthreemusketeer";
->>>>>>> 7f9691c0e5f9e891a1ef5f8ebc885c368e5abf8e
+    $database = "Noza";
 ?>
